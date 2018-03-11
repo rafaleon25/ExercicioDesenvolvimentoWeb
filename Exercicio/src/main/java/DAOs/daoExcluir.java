471a455d7d5f0b04ec340537b8f0f5e2cb4e441b
Josué <Josu@DESKTOP-RR5BE7B>
2018-03-11 15:48:29 -0300

    Correção Dao Excluir

--- Exercicio/src/main/java/DAOs/daoExcluir.java
+++ Exercicio/src/main/java/DAOs/daoExcluir.java
@@ -15,14 +15,17 @@ import java.sql.SQLException;
  * @author Josué
  */
 public class daoExcluir {
+
     public static void excluir(Integer id) throws SQLException, Exception {
 
-        String sql = "DELETE FROM PRODUTO_CATEGORIA WHERE ID_PRODUTO=?; DELETE FROM PRODUTO WHERE ID=?";
+        String sql = "DELETE FROM PRODUTO_CATEGORIA WHERE ID_PRODUTO=?";
+        String sql2 = "DELETE FROM PRODUTO WHERE ID=?";
         //Conexão para abertura e fechamento
         Connection connection = null;
         //Statement para obtenção através da conexão, execução de
         //comandos SQL e fechamentos
         PreparedStatement preparedStatement = null;
+        PreparedStatement preparedStatement2 = null;
         try {
             //Abre uma conexão com o banco de dados
             connection = ConnectionUtils.getConnection();
@@ -30,14 +33,21 @@ public class daoExcluir {
             preparedStatement = connection.prepareStatement(sql);
             //Configura os parâmetros do "PreparedStatement"
             preparedStatement.setInt(1, id);
-            preparedStatement.setInt(2, id);
 
             //Executa o comando no banco de dados
             preparedStatement.execute();
+
+            preparedStatement2 = connection.prepareStatement(sql2);
+            preparedStatement2.setInt(1, id);
+            preparedStatement2.execute();
+
         } finally {
             //Se o statement ainda estiver aberto, realiza seu fechamento
             if (preparedStatement != null && !preparedStatement.isClosed()) {
                 preparedStatement.close();
+            }
+            if (preparedStatement2 != null && !preparedStatement2.isClosed()) {
+                preparedStatement2.close();
             }
             //Se a conexão ainda estiver aberta, realiza seu fechamento
             if (connection != null && !connection.isClosed()) {
