fcbaae09a3f5f5db8c0fa124bc89650721d84cb3
Rafael Rodrigues <Rafael Rodrigues@DESKTOP-70O3HQ2>
2018-03-11 11:34:06 -0300

    alteração na servlet

--- Exercicio/src/main/java/Servlets/CadastroProdServlet.java
+++ Exercicio/src/main/java/Servlets/CadastroProdServlet.java
@@ -41,88 +41,104 @@ public class CadastroProdServlet extends HttpServlet {
             throws ServletException, IOException {
 
         HttpSession sessao = request.getSession();
+        Produto prod = new Produto();
+        ArrayList<Categoria> cat = new ArrayList<>();
+        Categoria cate = new Categoria();
+        Categoria cate2 = new Categoria();
+        Categoria cate3 = new Categoria();
+        Categoria cate4 = new Categoria();
+        Categoria cate5 = new Categoria();
 
         String nome = request.getParameter("nome-prod");
         String descricao = request.getParameter("descricao-prod");
-        String quantidade = request.getParameter("qtd-prod");
+        String quantidade = request.getParameter("quantidade-prod");
         String precoCompra = request.getParameter("preco-compra");
         String precoVenda = request.getParameter("preco-venda");
+
         String catego1 = request.getParameter("cat-1");
         String catego2 = request.getParameter("cat-2");
         String catego3 = request.getParameter("cat-3");
         String catego4 = request.getParameter("cat-4");
         String catego5 = request.getParameter("cat-5");
 
-        double precoComp = 0;
-        double precoVend = 0;
+        if (precoCompra.equals("")) {
+            request.setAttribute("erroCadastro", "Favor informar uma data de nascimento válida");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/CadastroProd.jsp");
+            dispatcher.forward(request, response);
+        }
+        if (precoVenda.equals("")) {
+            request.setAttribute("erroCadastro", "Favor informar uma data de nascimento válida");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/CadastroProd.jsp");
+            dispatcher.forward(request, response);
+        }
+
+        double precoC = 0;
+        double precoV = 0;
         int qtd = 0;
 
         try {
-            precoComp = Double.parseDouble(precoCompra);
-            precoVend = Double.parseDouble(precoVenda);
-        } catch (NumberFormatException e) {
-            e.getMessage();
+            precoC = Double.parseDouble(precoCompra);
+            precoV = Double.parseDouble(precoVenda);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         try {
-            qtd = Integer.parseInt(quantidade);
-        } catch (NumberFormatException e) {
-            e.getMessage();
-        }
-        ArrayList<Categoria> cat = new ArrayList<>();
-        Produto prod = new Produto();
-        prod.setNome(nome);
-        prod.setDescricao(descricao);
-        prod.setPrecoCompra(precoComp);
-        prod.setPrecoVenda(precoVend);
-        prod.setQuantidade(qtd);
 
-        Categoria cate = new Categoria();
-        cate.setNome(catego1);
-        Categoria cate2 = new Categoria();
-        cate2.setNome(catego2);
-        Categoria cate3 = new Categoria();
-        cate3.setNome(catego3);
-        Categoria cate4 = new Categoria();
-        cate4.setNome(catego4);
-        Categoria cate5 = new Categoria();
-        cate5.setNome(catego5);
+            qtd = Integer.parseInt(quantidade);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         if (catego1 != null) {
+            int categoria1 = Integer.parseInt(catego1);
+            cate.setId(categoria1);
             cat.add(cate);
         }
         if (catego2 != null) {
+            int categoria2 = Integer.parseInt(catego2);
+            cate2.setId(categoria2);
             cat.add(cate2);
         }
         if (catego3 != null) {
+            int categoria3 = Integer.parseInt(catego3);
+            cate3.setId(categoria3);
             cat.add(cate3);
         }
         if (catego4 != null) {
+            int categoria4 = Integer.parseInt(catego4);
+            cate4.setId(categoria4);
             cat.add(cate4);
         }
         if (catego5 != null) {
+            int categoria5 = Integer.parseInt(catego5);
+            cate5.setId(categoria5);
             cat.add(cate5);
         }
 
+        prod.setNome(nome);
+        prod.setDescricao(descricao);
+        prod.setQuantidade(qtd);
+        prod.setPrecoCompra(precoC);
+        prod.setPrecoVenda(precoV);
         prod.setCat(cat);
 
         try {
             Servico.CadastrarProduto(prod);
+            request.setAttribute("sucessCadastro", "Cadastro realizado com sucesso.");
+
+        } catch (ProdutoException | DataExceptions e) {
+            request.setAttribute("erroCadastro", e.getMessage());
             request.setAttribute("prodRepreenche", prod);
-        } catch (DataExceptions | ProdutoException e) {
-            request.setAttribute("prodErro", e);
-            System.out.println(e.getMessage());
-            
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/cadastrarProd.jsp");
-            dispatcher.forward(request, response);
         } catch (SQLException ex) {
             Logger.getLogger(CadastroProdServlet.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println(ex.getMessage());
         }
+        sessao.setAttribute("prod", prod);
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/cadastrarProd.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/CadastroProd.jsp");
         dispatcher.forward(request, response);
 
     }
-
 }
